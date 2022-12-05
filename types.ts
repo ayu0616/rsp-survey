3ba74f06ea1a99a90e70a975415cc83b4a3ca55d
ayu0616 <83691477+ayu0616@users.noreply.github.com>
2022-12-05 04:31:56 +0000

    ローカルストレージの型を定義

--- types.ts
+++ types.ts
@@ -1,8 +1,12 @@
 export type HandNum = 0 | 1 | 2;
 export type GenderNum = 0 | 1 | 2;
+export type ResultNum = 0 | 1 | 2;
 
 export type rspStatItem = {
     timestamp: Date;
-    hand: HandNum;
+    hand1: HandNum;
+    result1: ResultNum;
+    hand2: HandNum;
+    result2: ResultNum;
     gender: GenderNum;
 };
